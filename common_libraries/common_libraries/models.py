0ec5404e60f7ffb049ded068f203f44878b4c219
Arthur Grava <arthur.grava@gmail.com>
2021-01-03 19:03:11 +0100

    :sparkles: Using log to calculate RMSE as the house price competition does

--- common_libraries/common_libraries/models.py
+++ common_libraries/common_libraries/models.py
@@ -2,6 +2,7 @@ import pickle
 from typing import Any
 
 import pandas as pd
+import numpy as np
 from sklearn.metrics import mean_squared_error
 
 
@@ -11,6 +12,22 @@ def calculate_rmse(lr_model: Any, X: pd.DataFrame, Y: pd.DataFrame) -> float:
     """
     Y_pred = lr_model.predict(X)
     return mean_squared_error(Y, Y_pred, squared=False)
+
+
+def calculate_rmse_log(lr_model: Any, X: pd.DataFrame, Y: pd.DataFrame) -> float:
+    """
+    Calculates RMSE of the predictions applying log in the difference
+    """
+    Y_ini = pd.DataFrame(list(Y[Y.columns[0]]), columns=["Real"])
+    Y_pred = pd.DataFrame(lr_model.predict(X), columns=["Pred"])
+
+    df = pd.concat([Y_ini, Y_pred], axis=1)
+    df["log"] = df.apply(
+        lambda r: np.log( (r["Real"] - r["Pred"]) ** 2)
+        , axis=1
+    )
+
+    return (df["log"].sum() / df.shape[0]) ** (.5)
 
 
 def save(model: Any, model_name: str):
